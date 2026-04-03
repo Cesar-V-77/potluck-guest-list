725ab9bfc653ddaf2110d1e4de7582ac466d3b70
cesar_v <indigo.peacewaters@gmail.com>
2026-04-03 16:15:19 -0500

    Restructure the clear input box function expression

--- js/script.js
+++ js/script.js
@@ -18,9 +18,10 @@ addGuestButton.addEventListener("click", function () {
     let listItem = document.createElement("li");
     listItem.innerText = guest;
     guestList.append(listItem);
+    clearInput();
   }
-  const clearInput = function () {
-    guestInput.value = "";
-  };
-  clearInput();
 });
+
+const clearInput = function () {
+  guestInput.value = "";
+};
